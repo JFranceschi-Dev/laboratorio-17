c5b5cd637d13b914c677d56b6c211f5ce679ca1c
JFranceschi-Dev <jossettfranceschi@gmail.com>
2026-09-14 14:58:46 -0500

    fix func to test ci features

--- src/main/java/pa/gob/dntic/laboratorio17/Calculadora.java
+++ src/main/java/pa/gob/dntic/laboratorio17/Calculadora.java
@@ -2,7 +2,7 @@ package pa.gob.dntic.laboratorio17;
 
 public class Calculadora {
     public int sumar(int a, int b) {
-        return a + b +1;
+        return a + b;
     }
 
 //    public int restar(int a, int b) {
